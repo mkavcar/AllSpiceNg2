8bb0ba97fa95d5eb6efcf14fe433efedc4116fdb
mkavcar <mkavcar@gmail.com>
2016-04-26 21:26:38 -0400

    init app

---
+++ ember-cli-build.js
@@ -0,0 +1,13 @@
+/* global require, module */
+
+var Angular2App = require('angular-cli/lib/broccoli/angular2-app');
+
+module.exports = function(defaults) {
+  var app = new Angular2App(defaults, {
+    vendorNpmFiles: [
+      'angularfire2/**/*.js',
+      'firebase/lib/*.js'
+    ]
+  });
+  return app.toTree();
+}
